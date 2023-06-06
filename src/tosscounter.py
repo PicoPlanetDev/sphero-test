8ab2f1d63f03de6db83612aeaec36f08c652f97c
PicoPlanetDev <picoplanetdev@gmail.com>
2023-06-06 16:01:09 -0400

    Fixed tosscounter

--- src/tosscounter.py
+++ src/tosscounter.py
@@ -2,17 +2,20 @@ import time
 from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
 from spherov2.types import Color
+from spherov2.sphero_edu import EventType
+
+def on_freefall(sphero):
+    sphero.set_main_led_color(Color(r=255, g=0, b=0))
+
+def on_landing(sphero):
+    sphero.set_main_led_color(Color(r=0, g=255, b=0))
+    num_tosses += 1
+    sphero.set_matrix_character(str(num_tosses), Color(r=0, g=0, b=255))
+    if num_tosses == 10:
+        sphero.scroll_matrix_text('You win!', Color(r=0, g=0, b=255), 10, wait=True)
 
 toy = scanner.find_toy()
 with SpheroEduAPI(toy) as sphero:
     num_tosses = 0
-
-    def on_freefall(sphero):
-        sphero.set_main_led_color(Color(r=255, g=0, b=0))
-
-    def on_landing(sphero):
-        sphero.set_main_led_color(Color(r=0, g=255, b=0))
-        num_tosses += 1
-        sphero.set_matrix_character(str(num_tosses), Color(r=0, g=0, b=255))
-        if num_tosses == 10:
-            sphero.scroll_matrix_text('You win!', Color(r=0, g=0, b=255), 10, wait=True)
+    sphero.register_event(EventType.on_freefall, on_freefall)
+    sphero.register_event(EventType.on_landing, on_landing)
