9d1720adefca0bcd0eb81fe917062e2360a900b3
Sigmond Kukla <picoplanetdev@gmail.com>
2023-07-25 09:44:45 -0400

    Fixed merge conflicts

--- src/roll.py
+++ src/roll.py
@@ -3,29 +3,25 @@ from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
 from spherov2.types import Color
 
-TOY_NAME = "BB-EFF7"
+TOY_NAME = "SB-BFEA"
 
 toy = scanner.find_toy(toy_name=TOY_NAME)
 with SpheroEduAPI(toy) as sphero:
     sphero.set_main_led(Color(r=0, g=255, b=255))
     # Make a square
     # roll at 0 degrees at speed 100 for 1 second
-    sphero.roll(0, 255, 1)
-    time.sleep(1)
+    sphero.roll(0, 100, 1)
     # roll at 90 degrees at speed 100 for 1 second
-    sphero.roll(90, 255, 1)
-    time.sleep(1)
+    sphero.roll(90, 100, 1)
     # roll at 180 degrees at speed 100 for 1 second
-    sphero.roll(180, 255, 1)
-    time.sleep(1)
+    sphero.roll(180, 100, 1)
     # roll at 270 degrees at speed 100 for 1 second
-    sphero.roll(270, 255, 1)
-    time.sleep(1)
+    sphero.roll(270, 100, 1)
 
     # we can do better though
     # for loop
     for i in range(4):
-        sphero.roll(i * 90, 255, 1)
+        sphero.roll(i * 90, 100, 1)
         time.sleep(1)
 
     # What if we wanted to roll in any regular polygon?
@@ -35,7 +31,7 @@ with SpheroEduAPI(toy) as sphero:
     num_sides = 6
     angle = 360 / num_sides
     for i in range(num_sides):
-        sphero.roll(int(i * angle), 255, 0.7)
+        sphero.roll(int(i * angle), 100, 0.5)
         time.sleep(1)
 
     # let's go back and make a circle
